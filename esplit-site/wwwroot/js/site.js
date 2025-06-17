b1b5575eff42d9b8d641754a83f7e2d363b8d729
Rohan Navsariwala <rohanknavsariwala@gmail.com>
2025-06-18 01:01:26 +0530

    perticipant dropdown and listing basic functionality added for add split

--- esplit-site/wwwroot/js/site.js
+++ esplit-site/wwwroot/js/site.js
@@ -1,7 +1,14 @@
 ﻿const Split = {
 
+	// this one always stays constant 
 	allContacts: [],
+
+	// this one we update it every time we input something in searchbox
+	// this is the actual list we show in the drop down
 	filteredContacts: [],
+
+	// this is the list where we will be getting all the values from, {id and amount to be precise}
+	participantList: [],
 
 	viewSplitStatus: function (SplitID) {
 		$.ajax({
@@ -20,9 +27,13 @@
 		});
 	},
 
-	addSplit: function () {
+	addSplit: async function () {
 		let SplitModal = new bootstrap.Modal(document.getElementById("AddSplitModal"));
 		SplitModal.show();
+
+		//we have to initially populate the list
+		this.allContacts = await Contact.getContacts();
+		//further operations we carry out via event listeners
 	},
 
 	addUserToParticipantList: function () {
@@ -111,9 +122,100 @@
 
 			}
 		});
-	}
+	},
 
+	// This function is called when the user types in the search box, just to filter based on search input
+	updateAndBindFilteredContacts: function (searchTerm) {
+		this.filteredContacts = this.allContacts.filter(contact => {
+			return contact.userData.fullName.toLowerCase().includes(searchTerm.toLowerCase()) ||
+				contact.userData.userName.toLowerCase().includes(searchTerm.toLowerCase());
+		});
+
+		// Bind the filtered contacts to the dropdown in the ui
+		this.bindFilteredContactsToDDL();
+	},
+
+	// filtered list is then bound to the dropdown in ui
+	bindFilteredContactsToDDL: function () {
+		let dropdown = document.getElementById("ContactSelectionDropdown");
+		dropdown.innerHTML = "";
+		this.filteredContacts.forEach(contact => {
+			let item = document.createElement("li");
+			item.dataset.id = `FContactID${contact.userData.userID}`;
+			item.innerHTML = `
+				<div class="d-flex align-items-center gap-2">
+					<button type="button" class="btn btn-sm btn-success addPButton" onclick="Split.addToParticipantList(${contact.userData.userID})">&plus;</button>
+					<span>${contact.userData.fullName} <span class="text-muted">(${contact.userData.userName})</span></span>
+				</div>
+			`;
+
+			dropdown.appendChild(item);
+		});
+		if (this.filteredContacts.length === 0) {
+			let item = document.createElement("li");
+			item.value = "";
+			item.innerHTML = "No contacts found";
+			dropdown.appendChild(item);
+		}
+	},
+
+	// This function is called when the user clicks on the add button in the dropdown
+	addToParticipantList: function (contactID) {
+		let contact = this.allContacts.find(c => c.userData.userID === contactID);
+		this.participantList.push(contact);
+		this.bindParticipantsList();
+	},
+
+	// This function is called when the user clicks on the remove button in the participant list
+	removeFromParticipantList: function (contactID) {
+		let index = this.participantList.findIndex(c => c.userData.userID === contactID);
+		if (index !== -1) {
+			this.participantList.splice(index, 1);
+		}
+		let dataId = `FContactID${contactID}`;
+		let participantEntry = document.querySelector(dataId);
+		if (participantEntry) {
+			participantEntry.remove();
+		}
+		this.bindParticipantsList();
+	},
+
+	// This function binds the participant list to the UI, this generally happens after adding or removing a participant
+	bindParticipantsList: function () {
+		let listToPopulate = document.getElementById("ParticipantsList");
+		listToPopulate.innerHTML = "";
+		this.participantList.forEach(contact => {
+			let listItem = document.createElement("li");
+			listItem.setAttribute("data-participant-id", contact.userData.userID);
+			listItem.innerHTML = `
+				<div class="d-flex align-items-center gap-2 flex-wrap">
+					<span class="fw-bold">${contact.userData.fullName}</span>
+					<span class="text-muted">(${contact.userData.userName})</span>
+					<input type="number" data-calc-amount class="form-control form-control-sm text-muted" readonly style="width: 80px;" />
+					<input type="number" data-amount-id="Participant${contact.userData.userID}" class="form-control form-control-sm" value="0" style="width: 80px;" />
+					<button class="btn btn-sm btn-danger" onclick="Split.removeFromParticipantList(${contact.userData.userID})">&minus;</button>
+				</div>
+			`;
+
+			listToPopulate.appendChild(listItem);
+		});
+	}
 };
+
+// Initialize the Split object and populate all contacts on page load
+$('#ContactSelectionSearch').on('input', function () {
+	let searchTerm = $(this).val();
+	if (searchTerm.length >= 3) {
+		Split.updateAndBindFilteredContacts(searchTerm);
+	} if (searchTerm.length === 0) {
+		document.getElementById("ContactSelectionDropdown").innerHTML = "";
+	}
+});
+
+$('.addPButton').on('click', function () {
+	document.getElementById("ContactSelectionDropdown").innerHTML = "";
+});
+
 
 const Contact = {
 
@@ -122,17 +224,20 @@ const Contact = {
 	},
 
 	getContacts: function () {
-		$.ajax({
-			url: "Contacts/GetContacts",
-			type: "GET",
-			success: function (response) {
-				return response;
-			},
-			error: function (xhr){
-				Common.showErrorModal(xhr.responseText);
-				return [];
-			}
-		})
+		return new Promise(function (resolve, reject) {
+			$.ajax({
+				url: "Contacts/GetContacts",
+				type: "GET",
+				dataType: "json",
+				success: function (response) {
+					resolve(response);
+				},
+				error: function (xhr) {
+					Common.showErrorModal(xhr.responseText);
+					reject(xhr);
+				}
+			});
+		});
 	},
 
 	sendContactRequest: function () {
@@ -229,9 +334,9 @@ const Common = {
 	rePopulateContacts: function () {
 		let listToPopulate = document.getElementById("ParticipantSelectionListed");
 		
-		}
 	}
 }
+
 
 const Notify = {
 	deleteNotification: function (NotificationID) {
@@ -260,225 +365,3 @@ $("#ParticipantSelectionDropdown").on("change", () => {
 })
 
 //endregion
-
-function viewSplitStatus(SplitID) {
-	$.ajax({
-		url: "/Dashboard/SplitParticipants?SplitID=" + SplitID,
-		type: "GET",
-		success: function (response) {
-			document.getElementById("SplitParticipantsContainer").innerHTML = response;
-
-			let ParticipantsModal = new bootstrap.Modal(document.getElementById('SplitParticipantsModal'));
-			ParticipantsModal.show();
-
-		},
-		error: function (xhr, SplitStatus, error) {
-			console.log('Error:', error);
-		}
-	});
-}
-
-function AddNewSplit() {
-	let SplitModal = new bootstrap.Modal(document.getElementById("AddSplitModal"));
-	SplitModal.show();
-}
-
-function AddUserToParticipantList() {
-	let selection = $("#ParticipantSelectionDropdown option:selected");
-
-	let template = `<div class="list-group-item d-flex justify-content-between align-items-center Participant-Entry" id="Participant${selection.val()}">
-                        <div>
-                            <span>${selection.text()}</span>
-                            <input type="number" class="form-control d-inline-block ms-2" style="width: 100px;" />
-                            [<input type="number" class="text-muted amount" id="AmountOf${selection.val()}" readonly />]
-                        </div>
-                        <button type="button" class="btn btn-sm btn-danger" onclick="RemoveParticipant(${selection.val()})">&times;</button>
-                    </div>`;
-
-	let element = document.createElement("div");
-	element.innerHTML = template;
-	document.getElementById("ParticipantsList").appendChild(element);
-
-}
-
-function RemoveParticipant(ParticipantID) {
-	let ParticipantToRemove = `#Participant${ParticipantID}`;
-	$(ParticipantToRemove).remove();
-}
-
-function splitToggleActionPerformed(SplitID, SplitStatus) {
-	if (SplitStatus == 11) {
-		closeSplit(SplitID);
-	} else if (SplitStatus == 1) {
-		approveSplit(SplitID);
-	} else if (SplitStatus == 3) {
-		paySplit(SplitID);
-	}
-}
-
-function closeSplit(SplitID) {
-	console.log("close split " + SplitID)
-	$.ajax({
-		url: "/Dashboard/CloseSplit?SplitID=" + SplitID,
-		type: "DELETE",
-		success: function (response) {
-			
-		},
-		error: function (error) {
-
-		}
-	});
-}
-
-function approveSplit(SplitID) {
-	console.log("approve split " + SplitID)
-	$.ajax({
-		url: "/Dashboard/ApproveSplit?SplitID=" + SplitID,
-		type: "PATCH",
-		success: function (response) {
-
-		},
-		error: function (error) {
-
-		}
-	});
-}
-function rejectSplit(SplitID) {
-	console.log("reject split" + SplitID)
-	$.ajax({
-		url: "/Dashboard/RejectSplit?SplitID=" + SplitID,
-		type: "DELETE",
-		success: function (response) {
-
-		},
-		error: function (error) {
-
-		}
-	});
-}
-
-function paySplit(SplitID) {
-	console.log("pay split" + SplitID)
-	$.ajax({
-		url: "/Dashboard/PayDues?SplitID=" + SplitID,
-		type: "",
-		success: function (response) {
-
-		},
-		error: function (error) {
-
-		}
-	});
-}
-
-function deleteNotification(NotificationID) {
-	$.ajax({
-		url: "/Notifications?NotificationID=" + NotificationID,
-		type: "DELETE",
-		success: function (response) {
-			if (response.success = true) {
-				let NID = `#Notification-${NotificationID}`;
-				$(NID).remove();
-			} else {
-				showErrorModal(response.message);
-			}
-		},
-		error: function (error) {
-			showErrorModal(error);
-		}
-	});
-}
-
-function SendContactRequest() {
-	let toUserName = $("#toUserName").val();
-	console.log("contact req" + toUserName)
-	if (toUserName != "") {
-		$.ajax({
-			url: "/Contacts/SendRequest?toUserName=" + toUserName,
-			type: "POST",
-			success: function (response) {
-					$("#SentContactRequestContainer").append(response);
-			},
-			error: function (xhr) {
-				let err = JSON.parse(xhr.responseText);
-				if (err.success === false) {
-					showErrorModal(err.message);
-				}
-			}
-		});
-	} else {
-		showErrorModal("bro fill the input field")
-	}
-}
-
-function ApproveContactRequest(ContactID) {
-	$.ajax({
-		url: "/Contacts/ApproveRequest?ContactID=" + ContactID,
-		type: "PATCH",
-		success: function (response) {
-			if (response) {
-				let CID = `#Contact-${ContactID}`;
-				$(CID).remove();
-				console.log(response);
-				$("#MyContactsContainer").append(response);
-			} 
-		},
-		error: function (xhr) {
-			let err = JSON.parse(xhr.responseText);
-			if (err.success === false) {
-				showErrorModal(err.message);
-			}
-		}
-	});
-}
-function RejectContactRequest(ContactID) {
-	$.ajax({
-		url: "/Contacts/RejectRequest?ContactID=" + ContactID,
-		type: "PATCH",
-		success: function (response) {
-			if (response.success == true) {
-				let CID = `#Contact-${ContactID}`;
-				$(CID).remove();
-			} else {
-				showErrorModal(response.message);
-			}
-		},
-		error: function (xhr) {
-			showErrorModal(xhr.responseText.message);
-		}
-	});
-}
-function DeleteContact(ContactID, type) {
-	$.ajax({
-		url: "/Contacts/DeleteRequest?ContactID=" + ContactID + "&type=" + type,
-		type: "DELETE",
-
-		success: function (response) {
-			if (response.success == true) {
-				let CID = `#Contact-${ContactID}`;
-				$(CID).remove();
-			} else {
-				showErrorModal(response.message);
-			}
-		},
-		error: function (xhr) {
-			showErrorModal(xhr.responseText.message);
-		}
-	});
-}
- 
-function showErrorModal(message) {
-	document.getElementById('errorModalBody').innerText = message;
-	const modal = new bootstrap.Modal(document.getElementById('errorModal'));
-	modal.show();
-}
-
-<li>
-	<input type="hidden" value="${contact.userid}"></input>
-	<button class="btn btn-primary" onclick="addToParticipantList(${contact.userid})">+</button>
-	<span class="contact-name">${contact.fullname}</span>
-	<span class="contact-name">${contact.username}</span>
-	<input type="number" data-calc-amount="" class="text-muted amount " readonly />
-	<input type="number" data-raw-amount="" class="form-control d-inline-block ms-2" id="AmountOfContactID"/>"
-
-</li>
